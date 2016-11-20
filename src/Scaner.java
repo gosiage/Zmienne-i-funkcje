17ff6dac6cdebc878a131138b9628c93fd880b11
Małgorzata G <gosiage1@gmail.com>
2016-11-20 03:10:40 +0100

    Scaner

---
+++ src/Scaner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Scaner
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+	//	System.out.println(" Podaj imiê " );
+	//	String imiê = input.nextLine();
+	//	System.out.println("Witaj " + imiê);
+		
+		System.out.println("Podaj twójk wiek");
+		int wiek = input.nextInt();
+		System.out.println("Twoj Wiek to " + wiek);
+	}
+	
+	
+}
